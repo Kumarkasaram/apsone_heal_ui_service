9a83ef88e760723aea21e3d18c7897c0ad4b7f54
krithika.k <krithika.k@appnomic.com>
2021-10-05 16:00:40 +0530

    Swagger document generation changes.

--- src/main/java/com/heal/uiservice/controller/AccountController.java
+++ src/main/java/com/heal/uiservice/controller/AccountController.java
@@ -7,9 +7,7 @@ import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +22,7 @@ public class AccountController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully retrieved data"),
             @ApiResponse(code = 500, message = "Internal Server Error"),
             @ApiResponse(code = 400, message = "Invalid Request")})
-    @GetMapping(path = "/accounts")
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
     public ResponseEntity<ResponseBean<List<AccountBean>>> getAccountList(@ApiParam(value = "Api key", required = true)
                                                                           @RequestHeader(value = "Authorization") String auth) {
         ResponseBean<List<AccountBean>> response = getAccountsService.getAccountList(auth);
