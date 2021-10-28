05ddef1ea350825f353b796d2d1d7037f28c57fd
Sagala Prasad <prasad.s@appnomic.com>
2021-10-28 10:33:51 +0530

    Updated the pom.xml with build changes.

--- src/main/java/com/heal/dashboard/service/HealServiceMain.java
+++ src/main/java/com/heal/dashboard/service/HealServiceMain.java
@@ -3,14 +3,15 @@ package com.heal.dashboard.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@PropertySource(value="classpath:conf.properties")
 @ComponentScan
+@PropertySource(value="classpath:conf.properties")
 //@EnableTransactionManagement(proxyTargetClass = true)
 @Slf4j
 public class HealServiceMain {
